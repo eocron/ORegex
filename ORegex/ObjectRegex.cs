e20a6074ae4c12658813a6103cb484f1d0afc142
Артем Дорофеев <a.dorofeev@i-teco.ru>
2016-02-15 18:10:58 +0300

    Some refactoring.

--- ORegex/ObjectRegex.cs
+++ ORegex/ObjectRegex.cs
@@ -1,11 +1,6 @@
 ﻿using ORegex.Core.Parse;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Xml.Schema;
 using ORegex.Core;
 using ORegex.Core.FinitieStateAutomaton;
 
