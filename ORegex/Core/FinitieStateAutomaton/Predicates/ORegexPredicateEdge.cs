13c7bd7c4cbf3bc469b2e8a9ae61b719f6b92d72
eocron <ad-company@mail.ru>
2016-06-02 02:41:54 +0300

    Fix.

---
+++ ORegex/Core/FinitieStateAutomaton/Predicates/ORegexPredicateEdge.cs
@@ -0,0 +1,20 @@
+﻿namespace Eocron.Core.FinitieStateAutomaton.Predicates
+{
+    public sealed class ORegexPredicateEdge<TValue> : SystemPredicateEdge<TValue>
+    {
+        private bool IsNegative { get; set; }
+
+        private ORegex<TValue> _oregex { get; set; }
+
+        public ORegexPredicateEdge(string name, ORegex<TValue> regex, bool isNegative) : base(name, true)
+        {
+            IsNegative = isNegative;
+        }
+
+        public override bool IsMatch(SequenceHandler<TValue> values, int index)
+        {
+            var isMatch = _oregex.IsMatch(values.Collection, index);
+            return IsNegative ^ isMatch;
+        }
+    }
+}
